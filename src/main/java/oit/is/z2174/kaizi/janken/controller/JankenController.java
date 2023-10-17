d05cb15361da55827bf60272e4f58ecfccd7b5ee
B21102 <e1b21102@oit.ac.jp>
2023-10-17 15:04:28 +0900

    lec4前修正

---
+++ src/main/java/oit/is/z2174/kaizi/janken/controller/JankenController.java
@@ -0,0 +1,73 @@
+package oit.is.z2174.kaizi.janken.controller;
+
+import java.security.Principal;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.z2174.kaizi.janken.model.Janken;
+import oit.is.z2174.kaizi.janken.model.Entry;
+
+@Controller
+public class JankenController {
+  @Autowired
+  private Entry room;
+  // @GetMapping("/janken")
+  // public String janken() {
+  // return "janken.html";
+  // }
+
+  @PostMapping("/janken")
+  public String janken(@RequestParam String name, ModelMap model) {
+    model.addAttribute("name", name);
+    room.addUser(name);
+    return "janken.html";
+  }
+
+  @GetMapping("/janken")
+  public String sample38(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    model.addAttribute("name", loginUser);
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
+    return "janken.html";
+  }
+
+  @GetMapping("/game")
+  public String katimake(@RequestParam int n, ModelMap model) {
+    int c = 1;
+    Janken player = new Janken(n, c, null);
+    if (n == 1) {
+      model.addAttribute("player", "Gu");
+    } else if (n == 2) {
+      model.addAttribute("player", "Cyoki");
+    } else {
+      model.addAttribute("player", "Pa");
+    }
+    if (c == 1) {
+      model.addAttribute("cpu", "Gu");
+    } else if (c == 2) {
+      model.addAttribute("cpu", "Cyoki");
+    } else {
+      model.addAttribute("cpu", "Pa");
+    }
+
+    if (player.getkatimake() == -1) {
+      model.addAttribute("hantei", "You Lose!");
+    } else if (player.getkatimake() == 1) {
+      model.addAttribute("hantei", "You Win!");
+    } else {
+      model.addAttribute("hantei", "Aiko");
+    }
+
+    return "janken.html";
+  }
+}
---
+++ src/main/java/oit/is/z2174/kaizi/janken/model/Janken.java
@@ -0,0 +1,65 @@
+package oit.is.z2174.kaizi.janken.model;
+
+public class Janken {
+
+  int player;
+  int cpu;
+  int katimake;
+  String name;
+
+  // p1グーp2チョキp3パー
+
+  public Janken(int p, int c) {
+    katimake = this.hantei(p, c);
+  }
+
+  public Janken(String n) {
+    this.name = n;
+  }
+
+  public Janken(int p, int c, String n) {
+    katimake = this.hantei(p, c);
+    this.name = n;
+  }
+
+  public int hantei(int p, int c) {
+    if (p == 1) {
+      if (c == 1) {
+        return 0;
+      } else if (c == 2) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else if (p == 2) {
+      if (c == 1) {
+        return -1;
+      } else if (c == 2) {
+        return 0;
+      } else {
+        return 1;
+      }
+    } else if (p == 3) {
+      if (c == 1) {
+        return 1;
+      } else if (c == 2) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+    return 0;
+  }
+
+  public int getkatimake() {
+    return this.katimake;
+  }
+
+  public String getname() {
+    return this.name;
+  }
+
+  public void setname(String name) {
+    this.name = name;
+  }
+}
